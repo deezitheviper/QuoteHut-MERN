839a49e2ae703d560763a48b7aac7be39c6cfc65
deezitheviper <amuhdavid@gmail.com>
2022-11-17 20:02:59 +0100

    update on routes

--- api/routes/Quote.js
+++ api/routes/Quote.js
@@ -1,8 +1,30 @@
 import express from 'express';
-import { getQuotes } from '../controllers/quoteController.js';
+import { addQuote,deleteQuote,getQuote, getQuotes } from '../controllers/quoteController.js';
+import multer from 'multer';
+import cloudinary from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET, 
+  }); 
+
+export const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: "Dev",
+      allowedFormats: ['jpg','jpeg','png']
+    },
+  })
+
+
+const upload = multer()
 const router = express.Router();
 
 router.get('/', getQuotes)
+router.post('/create', addQuote)
+router.get('/:id', getQuote)
+router.delete('/:id', deleteQuote)
 
 export default router;
