0bccdaf8636842fb76ff7f9efe8051223789933a
deezitheviper <amuhdavid@gmail.com>
2022-11-18 23:20:43 +0100

    update on feed

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 import {Header } from '../components/Index';
 import MasonryGrid from '../components/Masonry';
+import instance from '../utils/axios';
+
 
 const Feed = () => {
     const [quotes, setQuotes] = useState();
@@ -13,25 +15,35 @@ const Feed = () => {
     const slugToText = (slug) => {
         return slug.toLowerCase().replace(/-/g,' ')
     }
+
+ 
+
     useEffect(()=> {
         setLoading(true)
         if(id){
-            
+             
+        }else{
+           instance.get('quote/').then(res =>{
+
+            setQuotes(res.data)
+            setLoading(false)
+           })
         }
-        setLoading(false)
+        
     },[id])
+
     if(loading) return <Spinner message="we are adding new details to your feed" />
 
 
     return (
         <div className='flex-1 px-2 md:px-5'>
-           {console.log(quotes)}
+          
             <div className='bg-gray-50'>
                 <Header />
             </div>
             <div className='h-full'>
                 
-            {quotes?.length > 0 ? <MasonryGrid quotes={quotes} />
+            {quotes?.length > 0 ? <MasonryGrid quotes={quotes}  />
             :<div className='flex font-bold text-gray-700 tex-2xl justify-center items-center'>Opps No Quotes yet, create one! </div>
             }
 
