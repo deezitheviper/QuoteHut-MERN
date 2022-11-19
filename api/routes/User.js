3bf02f7ba9606ce696bca918b196590e36087ab7
deezitheviper <amuhdavid@gmail.com>
2022-11-19 22:52:35 +0100

    update on user routes

--- api/routes/User.js
+++ api/routes/User.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { getUser } from '../controllers/userController.js';
+import { getSaveQ, getUser, getUserQ } from '../controllers/userController.js';
 const router = express.Router();
 
 router.get('/:userId', getUser)
+router.get('/:userId/quotes', getUserQ)
+router.get('/:userId/saved', getSaveQ)
 
 export default router;
