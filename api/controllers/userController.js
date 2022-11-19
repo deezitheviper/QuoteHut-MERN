db17bbfc89c7eb094be00336eaf1d41388320132
deezitheviper <amuhdavid@gmail.com>
2022-11-19 22:51:55 +0100

    user controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/authModel.js'
+import quote from '../models/quote.js'
 
 
 export const getUser = async (req, res, next) =>{
@@ -9,5 +10,26 @@ export const getUser = async (req, res, next) =>{
         res.status(200).json(user)
        }catch(err) {
         console.error(err)
+       }
+}
+
+export const getUserQ = async (req,res,next) =>{
+       const {userId} = req.params
+       try{
+              const quotes = await quote.find({postedBy:userId})
+              res.status(200).json(quotes)
+       }catch(err) {
+              console.log(err)
+       }
+}
+
+export const getSaveQ = async (req,res,next) => {
+       const {userId} = req.params
+       try {
+              const quotes = await quote.find({savedBy:userId})
+              quotes.save()
+              res.status(200).json(quotes)
+       }catch(err) {
+              console.log(err)
        }
 }
